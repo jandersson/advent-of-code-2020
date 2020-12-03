1981197581694a3eaf1639261601adb2ef81ed6a
Jonas Andersson <jonas@andersson.io>
2020-12-03 11:00:11 +0100

    Solve second part

--- 2/password_philosophy/src/main.rs
+++ 2/password_philosophy/src/main.rs
@@ -5,7 +5,7 @@ struct Password {
     policy_letter: char,
     text: String,
 }
-fn is_password_compliant(password: Password) -> bool {
+fn is_password_compliant(password: &Password) -> bool {
     let mut policy_counter = 0;
     for c in password.text.chars() {
         if c == password.policy_letter {
@@ -18,10 +18,26 @@ fn is_password_compliant(password: Password) -> bool {
     true
 }
 
+fn is_password_still_compliant(password: &Password) -> bool {
+    (password
+        .text
+        .chars()
+        .nth((password.policy_min - 1).into())
+        .unwrap()
+        == password.policy_letter)
+        ^ (password
+            .text
+            .chars()
+            .nth((password.policy_max - 1).into())
+            .unwrap()
+            == password.policy_letter)
+}
+
 fn main() {
     let filename = "input.txt";
 
     let mut num_valid_passwords = 0;
+    let mut num_still_valid_passwords = 0;
 
     fs::read_to_string(&filename)
         .expect("Could not open file.")
@@ -37,10 +53,17 @@ fn main() {
                 policy_letter: data[2].parse().expect("Not a character"),
                 text: data[3].to_string(),
             };
-            if is_password_compliant(password) {
+            if is_password_compliant(&password) {
                 num_valid_passwords += 1;
             };
+            if is_password_still_compliant(&password) {
+                num_still_valid_passwords += 1;
+            }
         });
 
     println!("Number of valid passwords: {}", num_valid_passwords);
+    println!(
+        "Number of passwords that are still valid: {}",
+        num_still_valid_passwords
+    );
 }
