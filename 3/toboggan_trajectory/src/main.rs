363a7cf940d8262bdd7d61ba394c8c6d12cc8287
Jonas Andersson <jonas@andersson.io>
2020-12-03 18:51:36 +0100

    Solve first part

--- 3/toboggan_trajectory/src/main.rs
+++ 3/toboggan_trajectory/src/main.rs
@@ -1,3 +1,19 @@
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut map: Vec<Vec<char>> = fs::read_to_string("input.txt")
+        .expect("Couldn't open the file!")
+        .lines()
+        .map(|line| line.trim().chars().collect())
+        .collect();
+
+    let mut num_trees = 0;
+    for (i, line) in map.iter_mut().enumerate() {
+        let line_len = line.len();
+        line.rotate_left((i * 3) % line_len);
+        if line[0] == '#' {
+            num_trees += 1;
+        }
+    }
+    println!("Number of trees: {}", num_trees);
 }
